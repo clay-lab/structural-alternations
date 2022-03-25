caaf249d14dfa30742dbcae4016d0cba8ffe2e18
mawilson1234 <michael.a.wilson@yale.edu>
2022-03-25 13:04:33 -0400

    update scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #SBATCH --job-name=testsalts
-#SBATCH --output=test_log.txt
+#SBATCH --output=logs/test_log.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --gpus=p100:3
 #SBATCH --partition=gpu
-#SBATCH --time=01:00
+#SBATCH --time=01:00:00
 
 module load CUDA
 module load cuDNN
 
-# using your anaconda environment
 source activate salts
+cd ..
 
-python ../tune.py -m \
+python tune.py -m \
 	hydra/launcher=joblib \
 	hydra.launcher.n_jobs=2 \
 	model=bert,distilbert,roberta \
-	tuning=glob(dative*,exclude=[*large,*all,*mail*,*passive]) \
+	'tuning=glob(dative*,exclude=[*large,*all,*mail*,*passive)' \
 	dev=best_matches \
 	dev_exclude=mail \
 	hyperparameters.max_epochs=10 \
