309ff675ff61568b4b732325b1aa6e7b5efc2f7a
mawilson1234 <michael.a.wilson@yale.edu>
2022-03-24 22:45:20 -0400

    update env, add scripts for grace testing

---
+++ scripts/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=testsalts
+#SBATCH --output=test_log.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --gpus=p100:3
+#SBATCH --partition=gpu
+#SBATCH --time=01:00
+
+module load CUDA
+module load cuDNN
+
+# using your anaconda environment
+source activate salts
+python ../tune.py -m \
+	hydra/launcher=joblib \
+	hydra.launcher.n_jobs=2 \
+	model=bert,distilbert,roberta \
+	tuning=glob(dative*,exclude=[*large,*all,*mail*,*passive]) \
+	dev=best_matches \
+	dev_exclude=mail \
+	hyperparameters.max_epochs=10 \
+	hyperparameters.strip_punct=false,true \
+	hyperparameters.masked_tuning_style=always,bert,none,roberta \
+	+use_gpu=true
