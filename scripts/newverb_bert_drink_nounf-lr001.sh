bca53fc2d817ebb00ce17000b2cb60b8173f92ee
mawilson1234 <michael.a.wilson@yale.edu>
2022-03-30 07:55:33 -0400

    add scripts'

---
+++ scripts/newverb_bert_drink_nounf-lr001.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=salts
+#SBATCH --output=joblogs/salts_newverb_drink_nounf_%j.txt
+#SBATCH --ntasks=1
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=p100:1
+#SBATCH --partition=gpu
+#SBATCH --time=00:30:00
+#SBATCH --mail-type=ALL
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate salts
+
+python tune.py \
+	model=bert \
+	tuning=newverb_transitive_ext \
+	tuning.which_args=drink \
+	hyperparameters.max_epochs=100 \
+	hyperparameters.unfreezing=none \
+	hyperparameters.lr=.001 \
+	hyperparameters.mask_args=true \
+	+debug=true \
+	+use_gpu=true
