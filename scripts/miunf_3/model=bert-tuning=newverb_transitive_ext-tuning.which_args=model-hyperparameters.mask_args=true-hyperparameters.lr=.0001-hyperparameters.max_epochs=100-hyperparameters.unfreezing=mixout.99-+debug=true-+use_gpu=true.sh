9f248aa0b6d5388ae5c1d318f8429e6cc6ee2378
Michael Wilson <michael.a.wilson@yale.edu>
2022-03-31 21:35:34 -0400

    updated scripts

---
+++ scripts/miunf_3/model=bert-tuning=newverb_transitive_ext-tuning.which_args=model-hyperparameters.mask_args=true-hyperparameters.lr=.0001-hyperparameters.max_epochs=100-hyperparameters.unfreezing=mixout.99-+debug=true-+use_gpu=true.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=miunf_3
+#SBATCH --output=joblogs/miunf_3_%j.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --gpus=p100:1
+#SBATCH --partition=gpu
+#SBATCH --time=00:10:00
+#SBATCH --mail-type=ALL
+
+module load CUDA
+module load cuDNN
+module load miniconda
+source activate salts
+
+python tune.py model=bert \
+	tuning=newverb_transitive_ext \
+	tuning.which_args=model \
+	hyperparameters.mask_args=true \
+	hyperparameters.lr=.0001 \
+	hyperparameters.max_epochs=100 \
+	hyperparameters.unfreezing=mixout.99 \
+	+debug=true \
+	+use_gpu=true
