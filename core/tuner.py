e2a2e1aed2a48b2e8d63f80d86c411b8913d6221
mawilson1234 <michael.a.wilson@yale.edu>
2022-05-01 23:50:34 -0400

    move tensors to cpu before saving

--- core/tuner.py
+++ core/tuner.py
@@ -716,6 +716,7 @@ class Tuner:
 		
 		# put tensors on cpu before saving
 		for c in summary.columns:
+			breakpoint()
 			if isinstance(summary[c][0],torch.Tensor):
 				summary[c] 	= [v.clone().detach().cpu() for v in summary[c]]
 		
